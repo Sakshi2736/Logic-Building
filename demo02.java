086fc2ea6d063f51e772827533b4a458b42ba438
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-02-19 00:31:38 +0530

    Add files via upload

---
+++ demo02.java
@@ -0,0 +1,6 @@
+class demo02{
+public static void main(String arge[]){
+int a = 23,b= 56;
+int c = a+ b;
+ 
+System.out.println(c);}}
---
+++ demo05.java
@@ -0,0 +1,6 @@
+class demo05{
+public static void main(String arge[]){
+int num1 = 35;
+int num2 = 6;
+int product =num1*num2;
+System.out.println("35 * 6 = " +product);}}
