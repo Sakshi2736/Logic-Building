f5ba0c5ef9dc78143eacbc76eb6c8b8ab5407702
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-02-19 01:43:05 +0530

    Add files via upload

---
+++ Ass_01/demo11.java
@@ -0,0 +1,14 @@
+class demo11{
+public static void main(String arge[]){
+int a = 34;
+int b = 44;
+int c = 77;
+if (a>b && a>c){
+System.out.println("a is largest no");
+}
+else if(b>a && b>c){
+System.out.println("b is largest no");
+}
+else{
+System.out.println("c is largest no");}
+}}
