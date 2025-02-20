7ae18fdb604839206ef62f06dba3e37dc00617ce
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-02-21 00:46:47 +0530

    Add files via upload

---
+++ Day_2/Day2_A1/S2_01.java
@@ -0,0 +1,20 @@
+class S2_01{
+	public static void main(String args[]){
+	int mrks = 85;
+	if(mrks>=90){
+		System.out.println("A");
+	}
+	else if(mrks>80 && mrks<90){
+		System.out.println("B");
+	}
+	else if(mrks>70 && mrks<80){
+		System.out.println("C");
+	}
+	else if(mrks>60 && mrks<70){
+		System.out.println("D");
+	}
+	else{
+		System.out.println("F");
+	}
+	}
+}
