180de59143e4633055b8fdd9b7f3c48b8841df53
Sakshi2736 <sakshianiljadhav@gmail.com>
2025-02-26 22:48:32 +0530

    Add files via upload

---
+++ Ass_03/Pattern2.java
@@ -0,0 +1,6 @@
+public class Pattern2 { 
+        public static void main(String[] args) {  
+            // Upper half of the pattern
+            for (int i = 1; i <= 5; i++) {  
+       
+            }}
